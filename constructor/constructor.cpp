38b9570d61f432cfacf5d5aac6d5073c89d6d2e7
rauda08 <raudajnnh08@gmail.com>
2023-05-15 14:23:11 +0700

    membuat metode di prosedur class

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -5,5 +5,14 @@ using namespace std;
 
 class mahasiswa {
 private:
+	int nim;
+	string nama;
+public:
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	void cetak();
 };
+
 
