d1d9d3c0a187b5eb3eadd721ec378c8420a03d8e
rauda08 <raudajnnh08@gmail.com>
2023-05-15 15:07:50 +0700

    fungsi utama (memanggil)

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -36,7 +36,11 @@ void angka::isiData() {
         cout << i << " = "; cin >> arr[i];
     }
     cout<<endl;
+}
 
+int main() {
+    angka belajarcpp(3); //Constructor Dipanggil
+    angka* ptrBelajarcpp = new angka(5); //Constructor Dipanggil
 }
 int main()
 {
